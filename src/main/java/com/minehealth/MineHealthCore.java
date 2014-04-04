3f3f3b6c13cbb3c5fbafa40fe5847f615754d2d9
ordros <reordros@gmail.com>
2014-04-04 13:53:43 +0900

    First Commit

---
+++ src/main/java/com/minehealth/MineHealthCore.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Favcastle on 2014/04/04.
+ */
+
+package com.minehealth;
+
+import cpw.mods.fml.common.*;
+import cpw.mods.fml.common.event.*;
+import cpw.mods.fml.common.Mod.*;
+@Mod(modid = MineHealthCore.MODID, name = MineHealthCore.MODID, version = MineHealthCore.VERSION)
+public class MineHealthCore {
+
+    public static final String MODID = "MineHealth";
+    public static final String VERSION = "1.0";
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+    @EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+}
