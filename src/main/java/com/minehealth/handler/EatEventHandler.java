00b8aace4c3cce264b7e3fbb705bab8baf04f40a
ordros <reordros@gmail.com>
2014-04-08 13:22:42 +0900

    Loginhandler

--- src/main/java/com/minehealth/handler/EatEventHandler.java
+++ src/main/java/com/minehealth/handler/EatEventHandler.java
@@ -3,6 +3,7 @@ package com.minehealth.handler;
 import com.minehealth.logbook.EatLogBook;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.item.ItemFood;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.event.entity.player.PlayerUseItemEvent.Finish;
 
 import java.util.List;
@@ -15,6 +16,7 @@ public class EatEventHandler {
     public void SomethingEaten(Finish event){
         if(event.item.getItem() instanceof ItemFood){
             logbook.addEatLog(event.item);
+            NBTTagCompound tag = event.entityPlayer.getEntityData();
         }
 
     }
---
+++ src/main/java/com/minehealth/handler/UserLoginHandler.java
@@ -0,0 +1,13 @@
+package com.minehealth.handler;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+
+public class UserLoginHandler {
+    @SubscribeEvent
+    public void LoadEatLog(EntityJoinWorldEvent event){
+        event.entity.
+
+    }
+
+}
